3c8ab8a01cede704d5e2870de356db2890e6f02d
a <a@a-QEMU-Virtual-Machine gmail.com>
2023-01-30 14:50:12 -0800

    adds code for lab2

--- tcp_client.cpp
+++ tcp_client.cpp
@@ -10,20 +10,26 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+using namespace std;
+
 int main(int argc, char const *argv[]) 
 { 
 	// check to see if user input is valid
 	char socket_read_buffer[1024];
+	for(int i = 0; i < sizeof(socket_read_buffer); i++)
+	{
+		socket_read_buffer[i] = '\0';
+	}
 	
 	// TODO: Fill out the server ip and port
-	std::string server_ip = "";
-	std::string server_port = "";
+	string server_ip = argv[1];
+	string server_port = argv[2];
 
 	int opt = 1;
 	int client_fd = -1;
 
 	// TODO: Create a TCP socket()
-
+	client_fd = socket(AF_INET, SOCK_STREAM, 0);	
 	// Enable reusing address and port
 	if (setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
 		return -1;
@@ -34,7 +40,6 @@ int main(int argc, char const *argv[])
 		printf("Error- Socket setup failed");
 		return -1;
 	}
-	
 	// Helping you out by pepping the struct for connecting to the server
 	struct addrinfo hints;
 	struct addrinfo *server_addr;
@@ -44,10 +49,26 @@ int main(int argc, char const *argv[])
 	getaddrinfo(server_ip.c_str(), server_port.c_str(), &hints, &server_addr);
 
 	// TODO: Connect() to the server (hint: you'll need to use server_addr)
+	int connectRes = connect(client_fd, server_addr->ai_addr, server_addr->ai_addrlen);
 	// TODO: Retreive user input
-	// TODO: Send() the user input to the server
-	// TODO: Recieve any messages from the server and print it here. Don't forget to make sure the string is null terminated!
+	string user_input;
+	//while(1) {
+		//cout << "in while\n";
+		getline(cin, user_input);
+		// TODO: Send() the user input to the server
+		//cout << "got input";
+		send(client_fd, user_input.c_str(), user_input.length(), 0);
+		//cout << "sent message";
+		// TODO: Recieve any messages from the server and print it here. Don't forget to make sure the string is null terminated!
+		recv(client_fd, socket_read_buffer, sizeof(socket_read_buffer), 0);
+		cout << socket_read_buffer << "\n";
+	//}
+	//cout << "exited the while loop";
 	// TODO: Close() the socket
-
+	close(client_fd);
 	return 0; 
 } 
+
+
+
+
